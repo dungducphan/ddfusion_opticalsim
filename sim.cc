bf02e7dd6dac7623e001f9a311512d3d2b1db663
dungducphan <brianp.dung@utexas.edu>
2022-02-10 12:06:44 -0600

    Main simulation file.

---
+++ sim.cc
@@ -0,0 +1,51 @@
+
+#include "detConstruction.hh"
+#include "actionInit.hh"
+
+#include "QGSP_BERT_HP.hh"
+#include "G4EmStandardPhysics.hh"
+#include "G4OpticalPhysics.hh"
+#include "G4RunManagerFactory.hh"
+#include "G4SteppingVerbose.hh"
+#include "G4UImanager.hh"
+#include "QBBC.hh"
+#include "G4VisExecutive.hh"
+#include "G4UIExecutive.hh"
+#include "Randomize.hh"
+
+int main(int argc,char** argv) {
+  G4UIExecutive* ui = nullptr;
+  if ( argc == 1 ) { ui = new G4UIExecutive(argc, argv); }
+
+  G4int precision = 4;
+  G4SteppingVerbose::UseBestUnit(precision);
+
+  auto* runManager = G4RunManagerFactory::CreateRunManager(G4RunManagerType::Default, 1);
+
+  runManager->SetUserInitialization(new detConstruction());
+  
+  G4VModularPhysicsList* physicsList = new QGSP_BERT_HP();
+  physicsList->RegisterPhysics(new G4EmStandardPhysics());
+  physicsList->RegisterPhysics(new G4OpticalPhysics());
+  runManager->SetUserInitialization(physicsList);
+
+  runManager->SetUserInitialization(new actionInit());
+
+  G4VisManager* visManager = new G4VisExecutive;
+  visManager->Initialize();
+
+  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+  
+  if ( ! ui ) {
+    G4String command = "/control/execute ";
+    G4String fileName = argv[1];
+    UImanager->ApplyCommand(command+fileName);
+  } else {
+    UImanager->ApplyCommand("/control/execute init_vis.mac");
+    ui->SessionStart();
+    delete ui;
+  }
+
+  delete visManager;
+  delete runManager;
+}
