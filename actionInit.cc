99a02a68c7afa1422e7813fcd45d8e0616310845
dungducphan <brianp.dung@utexas.edu>
2022-02-14 18:50:27 -0600

    Include the runAction class and take into account multithreaded run.

--- actionInit.cc
+++ actionInit.cc
@@ -1,10 +1,13 @@
 #include "actionInit.hh"
-#include "generator.hh"
 
 actionInit::actionInit() : G4VUserActionInitialization() {}
 
 actionInit::~actionInit() {}
 
+void actionInit::BuildForMaster() const {
+  SetUserAction(new runAction());
+}
 void actionInit::Build() const {
+  SetUserAction(new runAction());
   SetUserAction(new generator());
 }
