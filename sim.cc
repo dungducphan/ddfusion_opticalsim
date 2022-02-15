46aba05f343d1c72176dfa539da678a715fa5191
dungducphan <brianp.dung@utexas.edu>
2022-02-14 18:52:35 -0600

    Fix several issues including the scintillation photon spectrum and the logical skin surfaces of the active volumes.

--- sim.cc
+++ sim.cc
@@ -2,11 +2,17 @@
 #include "detConstruction.hh"
 #include "actionInit.hh"
 
+#ifdef G4MULTITHREADED
+#include "G4MTRunManager.hh"
+#else
+#include "G4RunManager.hh"
+#endif
+
 #include "QGSP_BERT_HP.hh"
 #include "G4EmStandardPhysics_option4.hh"
 #include "G4OpticalPhysics.hh"
-#include "G4RunManagerFactory.hh"
 #include "G4SteppingVerbose.hh"
+#include "G4VModularPhysicsList.hh"
 #include "G4UImanager.hh"
 #include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
@@ -18,14 +24,18 @@ int main(int argc,char** argv) {
   G4int precision = 4;
   G4SteppingVerbose::UseBestUnit(precision);
 
-  auto* runManager = G4RunManagerFactory::CreateRunManager(G4RunManagerType::Default, 1);
+#ifdef G4MULTITHREADED
+  G4MTRunManager* runManager = new G4MTRunManager();
+#else
+  G4RunManager* runManager = new G4RunManager();
+#endif
 
   auto detModel = new detConstruction();
-  detModel->SetOpticalDiagnostic(true);
+  // detModel->SetOpticalDiagnostic(true);
   runManager->SetUserInitialization(detModel);
   
   G4VModularPhysicsList* physicsList = new QGSP_BERT_HP();
-  physicsList->RegisterPhysics(new G4EmStandardPhysics_option4());
+  physicsList->ReplacePhysics(new G4EmStandardPhysics_option4());
   G4OpticalPhysics* opticalPhysics = new G4OpticalPhysics();
   auto opticalParams= G4OpticalParameters::Instance();
   opticalParams->SetWLSTimeProfile("delta");
@@ -38,8 +48,6 @@ int main(int argc,char** argv) {
   runManager->SetUserInitialization(physicsList);
 
   runManager->SetUserInitialization(new actionInit());
-
-  runManager->Initialize();
 
   G4VisManager* visManager = new G4VisExecutive;
   visManager->Initialize();
